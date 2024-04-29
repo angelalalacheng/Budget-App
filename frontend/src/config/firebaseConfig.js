814faf4137e28ff192fed6fec2f958997d21c8a1
angelalala <angela9004@gmail.com>
2024-04-29 09:53:10 -0700

    feat: env variable

---
+++ frontend/src/config/firebaseConfig.js
@@ -0,0 +1,29 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+import {
+  getAuth,
+  setPersistence,
+  browserLocalPersistence,
+} from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const database = getFirestore(app);
+const analytics = getAnalytics(app);
+
+// Enable persistent authentication state
+setPersistence(auth, browserLocalPersistence);
+
+export { auth, database };
+export default app;
